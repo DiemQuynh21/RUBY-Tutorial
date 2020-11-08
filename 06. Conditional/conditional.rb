a06d5bf634b2550462d61ebb7baa17b977756270
DiemQuynh21 <lediemquynh999@gmail.com>
2020-11-08 17:49:20 +0700

    conditional

---
+++ 06. Conditional/conditional.rb
@@ -0,0 +1,13 @@
+#If statement
+
+x = 1
+if x > 2 
+    puts "x is greater than number 2"
+elseif x <= 2 and x != 0
+    puts "x is 1 "
+else
+    puts "i can't  guess the number"
+end
+
+$debug = 0
+puts "debug\n" if $debug
